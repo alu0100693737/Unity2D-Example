678f53a52eca8b24360e1ce878aead8398b12929
alu0100693737 <alu0100693737@ull.edu.es>
2016-06-17 18:53:54 +0200

    Consiguiendo animacion salto

--- Assets/Script/controladorPersonaje.cs
+++ Assets/Script/controladorPersonaje.cs
@@ -4,23 +4,30 @@ using System.Collections;
 public class controladorPersonaje : MonoBehaviour {
 
     public float fuerzaSalto = 13f;
+
     private bool enSuelo = true;
     public Transform comprobadorSuelo;
-    float comprobadorRadio = 0.07f;
+    private float comprobadorRadio = 0.07f;
     public LayerMask mascaraSuelo;
 
     //se permite el doble salto
     private bool dobleSalto = false;
-	// Use this for initialization
+    private Animator animator;
+
+    void Awake() {
+        animator = GetComponent<Animator>();
+
+    }
+
+
 	void Start () {
 	
 	}
 
-    void FixedUpdate()
-    {
+    void FixedUpdate() {
         enSuelo = Physics2D.OverlapCircle(comprobadorSuelo.position, comprobadorRadio, mascaraSuelo);
-        if(enSuelo)
-        {
+        animator.SetBool("isGrounded", enSuelo);
+        if (enSuelo) {
             dobleSalto = false;
         }
     }
@@ -28,14 +35,14 @@ public class controladorPersonaje : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
         //si hemos tocado la pantalla
-        if((enSuelo || !dobleSalto) && Input.GetMouseButtonDown(0)) {
+        if ((enSuelo || !dobleSalto) && Input.GetMouseButtonDown(0)) {
 
             GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x, fuerzaSalto);
             GetComponent<Rigidbody2D>().AddForce(new Vector2(0, fuerzaSalto));
             if(!dobleSalto && !enSuelo) {
                 dobleSalto = true;
             }
-            Debug.Log("hola");
+            
         }
 	
 	}
